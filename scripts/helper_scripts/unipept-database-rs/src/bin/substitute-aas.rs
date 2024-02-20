3c9b5940b4b988b248699edde0cafaa4a43d2396
stijndcl <declercq.stijn@outlook.com>
2024-02-20 12:06:10 +0100

    Run lint and fmt

--- scripts/helper_scripts/unipept-database-rs/src/bin/substitute-aas.rs
+++ scripts/helper_scripts/unipept-database-rs/src/bin/substitute-aas.rs
@@ -1,15 +1,16 @@
 use anyhow::{Context, Result};
 
+use clap::Parser;
 use std::collections::HashMap;
 use std::io::BufRead;
 use std::path::PathBuf;
-use clap::Parser;
 use unipept_database::utils::files::open_read;
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     let peptides_infile = open_read(&args.peptides).context("Error opening peptides input file")?;
-    let sequences_infile = open_read(&args.sequences).context("Error opening sequences input file")?;
+    let sequences_infile =
+        open_read(&args.sequences).context("Error opening sequences input file")?;
 
     let mut peptides_reader = peptides_infile.lines();
     let mut sequences_reader = sequences_infile.lines();
@@ -44,11 +45,13 @@ fn main() -> Result<()> {
             let line = sequences_reader.next();
             let line = match line {
                 None => break,
-                Some(l) => l.context("Error reading line from sequences file")?
+                Some(l) => l.context("Error reading line from sequences file")?,
             };
 
             let (id, sequence) = line.split_once('\t').context("Error splitting line")?;
-            let id_parsed = id.parse().with_context(|| format!("Error parsing {id} as an integer"))?;
+            let id_parsed = id
+                .parse()
+                .with_context(|| format!("Error parsing {id} as an integer"))?;
 
             if sequence_starts_with(sequence, prefix) {
                 sequence_to_id.insert(sequence.to_string(), id_parsed);
@@ -61,8 +64,12 @@ fn main() -> Result<()> {
 
         if !current_peptide.is_empty() {
             let data: Vec<&str> = current_peptide.splitn(4, '\t').collect();
-            let eq_id = sequence_to_id.get(data[1]).with_context(|| format!("Missing sequence {}", data[1]))?;
-            let orig_id = sequence_to_id.get(data[2]).with_context(|| format!("Missing sequence {}", data[2]))?;
+            let eq_id = sequence_to_id
+                .get(data[1])
+                .with_context(|| format!("Missing sequence {}", data[1]))?;
+            let orig_id = sequence_to_id
+                .get(data[2])
+                .with_context(|| format!("Missing sequence {}", data[2]))?;
             println!("{}\t{}\t{}\t{}", data[0], eq_id, orig_id, data[3]);
 
             current_peptide = String::new();
@@ -75,14 +82,18 @@ fn main() -> Result<()> {
             let line = peptides_reader.next();
             let line = match line {
                 None => break,
-                Some(l) => l.context("Error reading line from peptides file")?
+                Some(l) => l.context("Error reading line from peptides file")?,
             };
 
             let data: Vec<&str> = line.splitn(4, '\t').collect();
 
             if eq_or_og_starts_with(data[1], data[2], prefix) {
-                let eq_id = sequence_to_id.get(data[1]).with_context(|| format!("Missing sequence {}", data[1]))?;
-                let orig_id = sequence_to_id.get(data[2]).with_context(|| format!("Missing sequence {}", data[2]))?;
+                let eq_id = sequence_to_id
+                    .get(data[1])
+                    .with_context(|| format!("Missing sequence {}", data[1]))?;
+                let orig_id = sequence_to_id
+                    .get(data[2])
+                    .with_context(|| format!("Missing sequence {}", data[2]))?;
 
                 println!("{}\t{}\t{}\t{}", data[0], eq_id, orig_id, data[3]);
             } else {
@@ -104,7 +115,7 @@ struct Cli {
     #[clap(long)]
     peptides: PathBuf,
     #[clap(long)]
-    sequences: PathBuf
+    sequences: PathBuf,
 }
 
 fn eq_or_og_starts_with(eq: &str, og: &str, character: char) -> bool {
@@ -116,7 +127,7 @@ fn sequence_starts_with(seq: &str, character: char) -> bool {
 
     // Special case: treat I-L as one
     if character == 'I' {
-        return 'I' <= first && first <= 'L';
+        return ('I'..='L').contains(&first);
     }
 
     first == character
